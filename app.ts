d177b1830f8d70fbc42dba4feecd0985d24336b9
Machahary07 <jeumachahary07@gmail.com>
2024-12-02 07:57:15 +0530

    leetcode dec 1 & 2

--- app.ts
+++ app.ts
@@ -17,3 +17,36 @@ function intro(name: string, age: number): string {
 }
 
 console.log(intro('Jeu',18));
+
+// ------------------- Dec 1 leetcode
+
+function checkIfExist(arr: number[]): boolean {
+    const seen = new Set<number>();
+
+    for (const num of arr) {
+        if (seen.has(num * 2) || (num % 2 === 0 && seen.has(num / 2))) {
+            return true;
+        }
+        seen.add(num);
+    }
+
+    return false;
+}
+
+console.log(checkIfExist([10, 2, 5, 3]));
+console.log(checkIfExist([7, 1, 14, 11]));
+console.log(checkIfExist([3, 1, 7, 11]));
+console.log(checkIfExist([0, 0]));
+console.log(checkIfExist([-2, 4, -8, -1]));
+
+// ------------- Leetcode Dec 2
+
+function isPrefixOfWord(sentence: string, searchWord: string): number {
+    const words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].startsWith(searchWord)) return i + 1;
+    }
+    return -1;
+}
+
+console.log(isPrefixOfWord('I love typescript with deno2','typescript'));
