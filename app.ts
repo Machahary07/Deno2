11cf6cf3fb2f68721c746cfdd3ec68143547a51d
Machahary07 <jeumachahary07@gmail.com>
2024-12-09 18:49:11 +0530

    Dec 9

--- app.ts
+++ app.ts
@@ -204,3 +204,17 @@ function maxTwoEvents(events: number[][]): number {
 };
 
 console.log("getMaxEventBefore [[1,3,2],[4,5,2],[2,4,3]]: ",maxTwoEvents([[1,3,2],[4,5,2],[2,4,3]]))
+
+//-----------------------Dec 9
+function isArraySpecial(nums: number[], queries: number[][]): boolean[] {
+    const prefixSum = [0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] % 2 !== nums[i - 1] % 2) {
+            prefixSum.push(prefixSum[prefixSum.length - 1]);
+        } else {
+            prefixSum.push(prefixSum[prefixSum.length - 1] + 1);
+        }
+    }
+
+    return queries.map(([from,to]) => prefixSum[from] === prefixSum[to]);
+};
